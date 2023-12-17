d025f3f695f3c1bbe116636f6da95163fee815a7
bpa34 <brett.atkinson@me.com>
2023-12-17 08:41:50 -0800

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Brett Atkinson on 12/17/23.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
